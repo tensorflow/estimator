1d380df9ef126746785b9a372949504d847bef2c
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-12 14:25:30 -0700

    Fix broken links to Premade Estimator guide.
    PiperOrigin-RevId: 243332634

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -413,7 +413,7 @@ class Estimator(object):
     Args:
       input_fn: A function that constructs the input data for evaluation. See
         [Premade Estimators](
-        https://tensorflow.org/guide/premade#create_input_functions)
+        https://tensorflow.org/guide/premade_estimators#create_input_functions)
         for more information. The
         function should construct and return one of the following:  * A
         `tf.data.Dataset` object: Outputs of `Dataset` object must be a tuple
