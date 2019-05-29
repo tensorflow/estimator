5344d8c4a019dc9059905f34b2048a688f6b1eb3
A. Unique TensorFlower <gardener@tensorflow.org>
2019-05-29 08:40:32 -0700

    Update estimator parsing utils to feature columns v2.
    {classifier|regressor}_parse_example_spec were failing with non-v1 columns
    such as SequenceNumericColumn.
    
    PiperOrigin-RevId: 250497484

--- tensorflow_estimator/python/estimator/canned/parsing_utils_test.py
+++ tensorflow_estimator/python/estimator/canned/parsing_utils_test.py
@@ -20,17 +20,19 @@ from __future__ import print_function
 
 from tensorflow.python.feature_column import feature_column_lib as fc
 from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import test_util
 from tensorflow.python.ops import parsing_ops
 from tensorflow.python.platform import test
 from tensorflow_estimator.python.estimator.canned import parsing_utils
 
 
-class ClassifierParseExampleSpec(test.TestCase):
+class BaseClassifierParseExampleSpec(object):
   """Tests tf.estimator.classifier_parse_example_spec."""
 
+  def __init__(self, parse_example_fn):
+    self._parse_example_fn = parse_example_fn
+
   def test_defaults(self):
-    parsing_spec = parsing_utils.classifier_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')], label_key='b')
     expected_spec = {
         'a': parsing_ops.FixedLenFeature((1,), dtype=dtypes.float32),
@@ -39,7 +41,7 @@ class ClassifierParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_string(self):
-    parsing_spec = parsing_utils.classifier_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         label_dtype=dtypes.string)
@@ -51,7 +53,7 @@ class ClassifierParseExampleSpec(test.TestCase):
 
   # TODO(ispir): test label_default_value compatibility with label_dtype
   def test_label_default_value(self):
-    parsing_spec = parsing_utils.classifier_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         label_default=0)
@@ -65,7 +67,7 @@ class ClassifierParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_weight_column_as_string(self):
-    parsing_spec = parsing_utils.classifier_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         weight_column='c')
@@ -77,7 +79,7 @@ class ClassifierParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_weight_column_as_numeric_column(self):
-    parsing_spec = parsing_utils.classifier_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         weight_column=fc.numeric_column('c'))
@@ -91,13 +93,13 @@ class ClassifierParseExampleSpec(test.TestCase):
   def test_label_key_should_not_be_used_as_feature(self):
     with self.assertRaisesRegexp(ValueError,
                                  'label should not be used as feature'):
-      parsing_utils.classifier_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')], label_key='a')
 
   def test_weight_column_should_not_be_used_as_feature(self):
     with self.assertRaisesRegexp(ValueError,
                                  'weight_column should not be used as feature'):
-      parsing_utils.classifier_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')],
           label_key='b',
           weight_column=fc.numeric_column('a'))
@@ -106,17 +108,47 @@ class ClassifierParseExampleSpec(test.TestCase):
     with self.assertRaisesRegexp(ValueError,
                                  'tf.feature_column.numeric_column'):
       not_a_numeric_column = 3
-      parsing_utils.classifier_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')],
           label_key='b',
           weight_column=not_a_numeric_column)
 
 
-class RegressorParseExampleSpec(test.TestCase):
+class ClassifierParseExampleSpecV2(BaseClassifierParseExampleSpec,
+                                   test.TestCase):
+
+  def __init__(self, methodName='runTest'):  # pylint: disable=invalid-name
+    test.TestCase.__init__(self, methodName)
+    BaseClassifierParseExampleSpec.__init__(
+        self, parsing_utils.classifier_parse_example_spec_v2)
+
+  def test_non_v1_feature_column(self):
+    parsing_spec = self._parse_example_fn(
+        feature_columns=[fc.sequence_numeric_column('a')], label_key='b')
+    expected_spec = {
+        'a': parsing_ops.VarLenFeature(dtype=dtypes.float32),
+        'b': parsing_ops.FixedLenFeature((1,), dtype=dtypes.int64),
+    }
+    self.assertDictEqual(expected_spec, parsing_spec)
+
+
+class ClassifierParseExampleSpecV1(BaseClassifierParseExampleSpec,
+                                   test.TestCase):
+
+  def __init__(self, methodName='runTest'):  # pylint: disable=invalid-name
+    test.TestCase.__init__(self, methodName)
+    BaseClassifierParseExampleSpec.__init__(
+        self, parsing_utils.classifier_parse_example_spec)
+
+
+class BaseRegressorParseExampleSpec(object):
   """Tests tf.estimator.classifier_parse_example_spec."""
 
+  def __init__(self, parse_example_fn):
+    self._parse_example_fn = parse_example_fn
+
   def test_defaults(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')], label_key='b')
     expected_spec = {
         'a': parsing_ops.FixedLenFeature((1,), dtype=dtypes.float32),
@@ -125,7 +157,7 @@ class RegressorParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_int64(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         label_dtype=dtypes.int64)
@@ -136,7 +168,7 @@ class RegressorParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_label_default_value(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         label_default=0.)
@@ -150,7 +182,7 @@ class RegressorParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_label_dimension(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         label_dimension=3)
@@ -161,7 +193,7 @@ class RegressorParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_weight_column_as_string(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         weight_column='c')
@@ -173,7 +205,7 @@ class RegressorParseExampleSpec(test.TestCase):
     self.assertDictEqual(expected_spec, parsing_spec)
 
   def test_weight_column_as_numeric_column(self):
-    parsing_spec = parsing_utils.regressor_parse_example_spec(
+    parsing_spec = self._parse_example_fn(
         feature_columns=[fc.numeric_column('a')],
         label_key='b',
         weight_column=fc.numeric_column('c'))
@@ -187,13 +219,13 @@ class RegressorParseExampleSpec(test.TestCase):
   def test_label_key_should_not_be_used_as_feature(self):
     with self.assertRaisesRegexp(ValueError,
                                  'label should not be used as feature'):
-      parsing_utils.regressor_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')], label_key='a')
 
   def test_weight_column_should_not_be_used_as_feature(self):
     with self.assertRaisesRegexp(ValueError,
                                  'weight_column should not be used as feature'):
-      parsing_utils.regressor_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')],
           label_key='b',
           weight_column=fc.numeric_column('a'))
@@ -202,10 +234,35 @@ class RegressorParseExampleSpec(test.TestCase):
     with self.assertRaisesRegexp(ValueError,
                                  'tf.feature_column.numeric_column'):
       not_a_numeric_column = 3
-      parsing_utils.regressor_parse_example_spec(
+      self._parse_example_fn(
           feature_columns=[fc.numeric_column('a')],
           label_key='b',
           weight_column=not_a_numeric_column)
+
+
+class RegressorParseExampleSpecV2(BaseRegressorParseExampleSpec, test.TestCase):
+
+  def __init__(self, methodName='runTest'):  # pylint: disable=invalid-name
+    test.TestCase.__init__(self, methodName)
+    BaseRegressorParseExampleSpec.__init__(
+        self, parsing_utils.regressor_parse_example_spec_v2)
+
+  def test_non_v1_feature_column(self):
+    parsing_spec = self._parse_example_fn(
+        feature_columns=[fc.sequence_numeric_column('a')], label_key='b')
+    expected_spec = {
+        'a': parsing_ops.VarLenFeature(dtype=dtypes.float32),
+        'b': parsing_ops.FixedLenFeature((1,), dtype=dtypes.float32),
+    }
+    self.assertDictEqual(expected_spec, parsing_spec)
+
+
+class RegressorParseExampleSpecV1(BaseRegressorParseExampleSpec, test.TestCase):
+
+  def __init__(self, methodName='runTest'):  # pylint: disable=invalid-name
+    test.TestCase.__init__(self, methodName)
+    BaseRegressorParseExampleSpec.__init__(
+        self, parsing_utils.regressor_parse_example_spec)
 
 
 if __name__ == '__main__':
