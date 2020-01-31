e8c6b08fd3ca2641c139bc1d043e1d9f27e2ad78
Rahul Huilgol <huilgolr@amazon.com>
2020-01-31 21:10:13 +0000

    Fix import rename

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -834,9 +834,9 @@ class Estimator(object):
           raise ValueError("Couldn't find trained model at {}.".format(
               self._model_dir))
 
-      final_export_dir = export_helpers.get_timestamped_export_dir(export_dir_base)
+      final_export_dir = export_lib.get_timestamped_export_dir(export_dir_base)
       if gfile.NeedsTempLocation(final_export_dir):
-        current_export_dir = export_helpers.get_temp_export_dir(final_export_dir)
+        current_export_dir = export_lib.get_temp_export_dir(final_export_dir)
       else:
         current_export_dir = final_export_dir
       builder = saved_model_builder.SavedModelBuilder(current_export_dir)
