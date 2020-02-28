233f86dc7e5e1c0cb2054748b39628200a3e9575
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-02-28 23:56:00 +0530

    Resolving pylint error

--- tensorflow_estimator/python/estimator/keras.py
+++ tensorflow_estimator/python/estimator/keras.py
@@ -37,10 +37,12 @@ _DEFAULT_SERVING_KEY = tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
 
 class FormattedKeyError(KeyError):
   """KeyError with formatted error message.
-  
-  Python's `KeyError` has special casing around formatting (see https://bugs.python.org/issue2651). Use this class when the error message has newlines and other special format characters.
-  
-  Needed by https://github.com/tensorflow/tensorflow/issues/36857.  
+
+  Python's `KeyError` has special casing around formatting
+  (see https://bugs.python.org/issue2651). Use this class when the error
+  message has newlines and other special format characters.
+
+  Needed by https://github.com/tensorflow/tensorflow/issues/36857.
   """
   def __init__(self, message):
     self.message = message
