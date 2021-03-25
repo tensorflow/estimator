1c42ace1e2ab111c133bebcbe77a1bdac22ee6ca
Tayo Oguntebi <tayo@google.com>
2021-03-25 10:06:15 -0700

    Fixes typo in warning for TPUEstimator eval with more than one host.
    PiperOrigin-RevId: 365064828

--- tensorflow_estimator/python/estimator/tpu/tpu_context.py
+++ tensorflow_estimator/python/estimator/tpu/tpu_context.py
@@ -762,10 +762,9 @@ class _InternalTPUContext(object):
             'mode: {}; num_hosts: {}; num_replicas=1:{}'.format(
                 self._config.tpu_config.per_host_input_for_training, num_hosts,
                 num_replicas))
-      if num_replicas > 1 and self.is_input_per_host_with_iterators():
+      if num_hosts > 1 and self.is_input_per_host_with_iterators():
         tf.compat.v1.logging.warn('Running TPUEstimator.evaluate for input mode'
-                                  ' PER_HOST_V2 and num_hosts %d',
-                                  self.num_replicas)
+                                  ' PER_HOST_V2 and num_hosts %d', num_hosts)
     else:
       assert mode == model_fn_lib.ModeKeys.PREDICT
       if self._predict_batch_size is None:
