d2d9dfaff4136a8fefc7eb2da93accbbeafef4bf
Anmol-Sharma <anmolsharma010@gmail.com>
2021-04-30 15:41:35 +0200

    Fix for the merge conflict here : https://github.com/tensorflow/estimator/pull/59/conflicts

--- tensorflow_estimator/python/estimator/head/base_head.py
+++ tensorflow_estimator/python/estimator/head/base_head.py
@@ -906,6 +906,9 @@ def create_estimator_spec_train_op(
         if loss_reduction == losses_utils.ReductionV2.SUM_OVER_BATCH_SIZE:
           regularized_training_loss = losses_utils.scale_loss_for_distribution(
               regularized_training_loss)
+          num_replicas = tf.distribute.get_strategy().num_replicas_in_sync
+          if num_replicas > 1:
+            regularized_training_loss *= (1. / num_replicas)
         # For Optimizer of type tf.compat.v1.train.Optimizer
         if isinstance(optimizer, tf.compat.v1.train.Optimizer):
           train_op = optimizer.minimize(regularized_training_loss, var_list = trainable_variables)
