3e2ef1f74cf6b827ee297650087ed660f68c3488
Scott Zhu <scottzhu@google.com>
2021-10-14 15:58:06 -0700

    Remove the legacy directory of tensorflow/python/keras/estimator
    PiperOrigin-RevId: 403208301

--- tensorflow_estimator/python/estimator/keras_lib.py
+++ tensorflow_estimator/python/estimator/keras_lib.py
@@ -516,7 +516,7 @@ def model_to_estimator(keras_model=None,
                        use_v2_estimator=False,
                        metric_names_map=None,
                        export_outputs=None):
-  # LINT.ThenChange(//tensorflow/python/keras/estimator/__init__.py)
+  # LINT.ThenChange(//keras/estimator/__init__.py)
   """Constructs an `Estimator` instance from given keras model.
 
   If you use infrastructure or other tooling that relies on Estimators, you can
