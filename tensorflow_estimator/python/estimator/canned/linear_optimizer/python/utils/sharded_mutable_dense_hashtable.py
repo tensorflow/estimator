5f82e57ea7fee2466853b6cfbf15e02a8a497cb5
Daniel Ng <dnlng@google.com>
2022-12-07 05:51:59 -0800

    Migrate to TF2 Trackable API
    PiperOrigin-RevId: 493580843

--- tensorflow_estimator/python/estimator/canned/linear_optimizer/python/utils/sharded_mutable_dense_hashtable.py
+++ tensorflow_estimator/python/estimator/canned/linear_optimizer/python/utils/sharded_mutable_dense_hashtable.py
@@ -26,8 +26,10 @@ from tensorflow.python.framework import ops
 from tensorflow.python.ops import gen_lookup_ops
 from tensorflow.python.ops import lookup_ops
 from tensorflow.python.training.saver import BaseSaverBuilder
+from tensorflow.python.checkpoint import saveable_compat
 
 
+@saveable_compat.legacy_saveable_name("table")
 class _MutableDenseHashTable(lookup_ops.LookupInterface):
   """Copy of tf.contrib.lookup.MutableDenseHashTable."""
 
@@ -209,12 +211,15 @@ class _MutableDenseHashTable(lookup_ops.LookupInterface):
 
     return exported_keys, exported_values
 
-  def _gather_saveables_for_checkpoint(self):
-    """For object-based checkpointing."""
-    return {
-        "table": functools.partial(
-            _MutableDenseHashTable._Saveable, table=self)
-    }
+  def _serialize_to_tensors(self):
+    tesnors = self.export()
+    return {"-keys": tesnors[0], "-values": tesnors[1]}
+
+  def _restore_from_tensors(self, restored_tensors):
+    with ops.colocate_with(self.resource_handle):
+      return gen_lookup_ops.lookup_table_import_v2(self.resource_handle,
+                                                   restored_tensors["-keys"],
+                                                   restored_tensors["-values"])
 
   class _Saveable(BaseSaverBuilder.SaveableObject):
     """SaveableObject implementation for _MutableDenseHashTable."""
