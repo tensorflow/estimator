8f4e954bd562514dea702894f706515ec2d721c8
A. Unique TensorFlower <gardener@tensorflow.org>
2023-01-05 06:56:36 -0800

    Add `estimator_export` module to `tensorflow_estimator`.
    `estimator_export` contains a decorator (also `estimator_export`) which
    is a drop-in replacement for the `tf_export.estimator_export` decorator
    provided by the `tensorflow.python.util` package.
    
    PiperOrigin-RevId: 499860500

---
+++ tensorflow_estimator/python/estimator/estimator_export.py
@@ -0,0 +1,83 @@
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Utilities for exporting TensorFlow Estimator symbols to the API.
+
+Exporting a function or a class:
+
+To export a function or a class use the estimator_export decorator. For e.g.:
+```python
+@estimator_export('foo', 'bar.foo')
+def foo(...):
+  ...
+```
+
+If a function is assigned to a variable, you can export it by calling
+estimator_export explicitly. For e.g.:
+```python
+foo = get_foo(...)
+estimator_export('foo', 'bar.foo')(foo)
+```
+
+
+Exporting a constant
+```python
+foo = 1
+estimator_export('consts.foo').export_constant(__name__, 'foo')
+```
+"""
+
+from tensorflow.python.util import deprecation
+from tensorflow.python.util import tf_export
+
+
+class estimator_export(tf_export.api_export):  # pylint: disable=invalid-name
+  """Provides ways to export symbols to the TensorFlow Estimator API."""
+
+  def __init__(self, *args, **kwargs):  # pylint: disable=g-doc-args
+    """Export under the names *args (first one is considered canonical).
+
+    All symbols exported by this decorator are exported under the `estimator`
+    API name.
+
+    Args:
+      *args: API names in dot delimited format.
+      **kwargs: Optional keyed arguments.
+        v1: Names for the TensorFlow V1 API. If not set, we will use V2 API
+          names both for TensorFlow V1 and V2 APIs.
+        overrides: List of symbols that this is overriding
+          (those overrided api exports will be removed). Note: passing overrides
+          has no effect on exporting a constant.
+        allow_multiple_exports: Allow symbol to be exported multiple time under
+          different names.
+    """
+    kwargs['api_name'] = tf_export.ESTIMATOR_API_NAME
+    super().__init__(*args, **kwargs)
+
+  def __call__(self, func):
+    """Calls this decorator.
+
+    Args:
+      func: decorated symbol (function or class).
+
+    Returns:
+      The input function with _tf_api_names attribute set and marked as
+      deprecated.
+
+    Raises:
+      SymbolAlreadyExposedError: Raised when a symbol already has API names
+        and kwarg `allow_multiple_exports` not set.
+    """
+    func = deprecation.deprecated(None, 'Use tf.keras instead.')(func)
+    return super().__call__(func)
---
+++ tensorflow_estimator/python/estimator/estimator_export_test.py
@@ -0,0 +1,66 @@
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""estimator_export tests."""
+
+import sys
+
+from tensorflow.python.platform import test
+from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.util import tf_export
+# pylint: disable=g-deprecated-tf-checker
+from tensorflow_estimator.python.estimator import estimator_export
+
+
+class TestClass(object):
+  pass
+
+
+class ValidateExportTest(test.TestCase):
+  """Tests for estimator_export class."""
+
+  def setUp(self):
+    super().setUp()
+    self._modules = []
+
+  def tearDown(self):
+    super().tearDown()
+    for name in self._modules:
+      del sys.modules[name]
+    self._modules = []
+    if hasattr(TestClass, '_estimator_api_names'):
+      del TestClass._estimator_api_names
+    if hasattr(TestClass, '_estimator_api_names_v1'):
+      del TestClass._estimator_api_names_v1
+
+  def testExportClassInEstimator(self):
+    estimator_export.estimator_export('estimator.TestClass')(TestClass)
+    self.assertNotIn('_tf_api_names', TestClass.__dict__)
+    self.assertEqual(['estimator.TestClass'], tf_export.get_v1_names(TestClass))
+
+  @test.mock.patch.object(logging, 'warning', autospec=True)
+  def testExportDeprecated(self, mock_warning):
+    export_decorator = estimator_export.estimator_export('estimator.TestClass')
+    export_decorator(TestClass)
+
+    # Deprecation should trigger a runtime warning
+    TestClass()
+    self.assertEqual(1, mock_warning.call_count)
+    # Deprecation should only warn once, upon first call
+    TestClass()
+    self.assertEqual(1, mock_warning.call_count)
+
+
+if __name__ == '__main__':
+  test.main()
