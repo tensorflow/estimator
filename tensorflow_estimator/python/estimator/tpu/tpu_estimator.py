22846ec3da9cd0e868d249edb643a0276f601cbc
Mihai Maruseac <mihaimaruseac@google.com>
2023-05-02 07:25:24 -0700

    Update tensorflow_estimator/python/estimator/tpu/tpu_estimator.py

--- tensorflow_estimator/python/estimator/tpu/tpu_estimator.py
+++ tensorflow_estimator/python/estimator/tpu/tpu_estimator.py
@@ -310,7 +310,7 @@ class TPUEstimatorSpec(model_fn_lib._TPUEstimatorSpec):  # pylint: disable=prote
   executed on the CPU on every step, so there is communication overhead when
   sending tensors from TPU to CPU. To reduce the overhead, try reducing the
   size of the tensors. The `tensors` are concatenated along their major (batch)
-  dimension, and so must be >= rank 1.  The `host_call` is useful for writing
+  dimension, and so must be >= rank 1. The `host_call` is useful for writing
   summaries with `tf.summary.create_file_writer`.
 
   @compatibility(TF2)
